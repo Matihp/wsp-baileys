567b55db223e1036548891dcc1258449944b0795
matias1364 <matiasleoncontreras@gmail.com>
2023-05-24 21:40:29 -0300

    change event

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword, EVENTS, ProviderClass } = require('@bot-whatsapp/bot')
 
 const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
+const fs = require('fs')
+const fsP = require('fs/promises')
+const path=require('path')
 
-let fechaFinal;
+let fechaFinal,fechaInicial;
+const rutaJson = path.join(__dirname, './data.json')
+
+const leerJson=async()=>{
+    const data = await fsP.readFile(rutaJson)
+    const dataP = JSON.parse(data)
+    return dataP;
+}
+
+const escribirJson=(data)=>{
+    const updatedJsonData = JSON.stringify(data); // Convierte el JSON actualizado en una cadena
+    fs.writeFileSync('data.json', updatedJsonData);
+    return updatedJsonData;
+}
 
 const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(
     ['📄 Aquí tenemos el flujo secundario'],
-    null,
-    (ctx)=>{
+    {
+        delay:2000,
+    },
+    async (ctx,{provider})=>{
         fechaFinal = new Date();
-        
-    }
-    )
+        fechaFinal.setMinutes(fechaFinal.getMinutes() + 2);
+        const data = await leerJson();
+            for (const item of data) {
+                if (item.num == ctx.from) {
+                    item.dateFinal = fechaFinal;
+                    escribirJson(data)
+                }    
+            }
+    })
 
 const FlowNuevos = addKeyword(['1','Primero']).addAnswer(
     [
@@ -58,13 +82,29 @@ const flowConsultas = addKeyword(['3','Consultas']).addAnswer(
 )
 
 const flowPrincipal = addKeyword(EVENTS.WELCOME) 
-    .addAction(async (ctx, { flowDynamic, endFlow }) => {
-            let date = new Date()
+    .addAction(async (ctx, { flowDynamic, endFlow , provider }) => {
+            fechaInicial = new Date()
+            const data = await leerJson()
+            const a = await provider.getInstance();
+            await a.sendPresenceUpdate('composing',ctx.key.remoteJid)
             
-            if (fechaFinal != undefined)
-            return endFlow();
+            for (const item of data) {
+                if (item.num == ctx.from) {
+                    item.dateInicial = fechaInicial;
+                    const fecha1 = new Date(item.dateInicial)
+                    const fecha2 = new Date(item.dateFinal)
+
+                    if (fechaFinal != undefined && fecha1 < fecha2){     
+                        console.log('llego')
+                        return endFlow();
+                    }
+                    return;  
+                }
+            }
+            data.push({ num: ctx.from, dateInicial: fechaInicial, dateFinal: "" }); // Agrega el nuevo número al final del JSON
+            escribirJson(data)
         })
-    .addAnswer('🙌 Hola bienvenido a la clínica de Leonardo')
+    .addAnswer(['🙌 Hola bienvenido a la clínica de Leonardo'],{delay:3200,},null)
     
     .addAnswer(
         [
@@ -74,12 +114,13 @@ const flowPrincipal = addKeyword(EVENTS.WELCOME)
             '👉 *4* Recetas',
             '\nResponda con el numero de la opcion!',
         ],
-        null,
+        {delay:5200,},
         null,
         [FlowNuevos, flowPrevios, flowTuto, flowConsultas]
     )
 
 const main = async () => {
+
     const adapterDB = new MockAdapter()
     const adapterFlow = createFlow([flowPrincipal])
     const adapterProvider = createProvider(BaileysProvider)
